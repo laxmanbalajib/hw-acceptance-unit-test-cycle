3423222d5d4961ea2c09e93349321d0794e23c98
Laxman Balaji Balasubramanian <laxmanbalajib@tamu.edu>
2020-10-11 04:27:43 +0000

    Part2 complete

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,5 +1,14 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings
+    def self.all_ratings
     %w(G PG PG-13 NC-17 R)
-  end
+    end
+    def self.find_similar_movies(id)
+     movie=self.find(id)
+     if(movie.director==nil || movie.director.length==0)
+       return [Array([]), true, movie]
+     else
+       moviesall=Movie.where(director: movie.director)
+       return [moviesall, false, movie]
+     end
+    end
 end
